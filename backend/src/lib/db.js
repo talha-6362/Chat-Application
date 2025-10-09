636d13d3c760b7c2c6773d681b8c5c447b571c4e
Talha <you@example.com>
2025-10-09 21:07:22 +0500

    signup controller added

--- backend/src/lib/db.js
+++ backend/src/lib/db.js
@@ -1,11 +1,15 @@
 import mongoose from "mongoose";
 
-export const connectDB = async (mongoURI) => {
-    try {
-        const conn=await mongoose.connect(process.env.MONGO_URI)
-        console.log("MongoDB connected", conn.connection.host);
-    } catch (error) {
-        console.log("MongoDB connection error:", error.message);
-        process.exit(1);
+export const connectDB = async () => {
+  try {
+    if (!process.env.MONGODB_URI) {
+      throw new Error("MONGODB_URI not found in environment variables");
     }
+
+    const conn = await mongoose.connect(process.env.MONGODB_URI);
+    console.log(" MongoDB Connected:", conn.connection.host);
+  } catch (error) {
+    console.error(" MongoDB connection error:", error.message);
+    process.exit(1);
+  }
 };
