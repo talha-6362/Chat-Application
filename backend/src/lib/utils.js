8a3bbdc412cc41878b1816f0ccc69cce4b8aff41
Talha <you@example.com>
2025-10-10 17:16:59 +0500

    Update

--- backend/src/lib/utils.js
+++ backend/src/lib/utils.js
@@ -9,7 +9,7 @@ export const generateToken = (userId,res) => {
     res.cookie('jwt', token, {
         httpOnly: true,
         sameSite: 'strict',
-        maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
+        maxAge: 7 * 24 * 60 * 60 * 1000, 
         secure: process.env.NODE_ENV === 'development' ? false : true,
     });
     return token;
